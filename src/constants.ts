7f60ff404c2d22221c0dd6ca681620195ee5eadb
Caleb Adepitan <calebpitan@outlook.com>
2020-08-29 12:42:15 +0100

    Setup React environment

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const Warning = {
+  NO_TRANSFORM: `There's no \`transform\` callback given to the generated function from the \`useSlider\` hook.`,
+
+  NO_SLIDER: `The \`useScrollButton\` hook could not look up the appropraite \`Slider\` it expects. Ensure a \`Slider\` is rendered as the next sibling to the previous button, and as the previous sibling to the next button.`,
+
+  NO_SLIDER_CHILD: `The rendered \`Slider\` has no child component(s) that could be used to infer area and space calculations. Render some children like a Card component or something to \`Slider\`.`
+}
